72094e2369ac5383595c14b3439d160c03137953
Jayjay222 <88888683+Jayjay222@users.noreply.github.com>
2021-08-13 21:20:26 +0700

    Update bai1.js

--- bai1.js
+++ bai1.js
@@ -1,6 +1,6 @@
 function reverseInPlace(str) {
     var words = [];
-    words = str.split("\s+");
+    words = str.split("\s+")
     var result = "";
     for (var i = 0; i < words.length; i++) {
         return result += words[i].split('').reverse().join('');
